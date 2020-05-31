2f1c94245af22a311a685f2ee10be23628b1e6aa
Gatroxm <tavoxpau@gmail.com>
2020-05-31 14:12:22 -0500

    Variable de entorno

--- backend/server/config/config.js
+++ backend/server/config/config.js
@@ -14,7 +14,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/App_tareas';
 } else {
-    urlDB = 'mongodb+srv://root:ZGfnkjgYA0XhfYHW@cluster0-hxmfr.mongodb.net/AppTareas';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URL_DB = urlDB;
